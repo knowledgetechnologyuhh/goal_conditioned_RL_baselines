a809c0b8bfb87ff0cc97106eed5cb117d9e048d0
eppe <eppe@informatik.uni-hamburg.de>
2018-10-16 15:36:10 +0200

    Continuing stuff!

--- baselines/model_based/model_rnn.py
+++ baselines/model_based/model_rnn.py
@@ -3,6 +3,7 @@ from baselines.util import store_args, nn
 
 
 class ModelRNN:
+
     @store_args
     def __init__(self, inputs_tf):
         """The actor-critic network and related training code.
@@ -14,14 +15,15 @@ class ModelRNN:
             dimu (int): the dimension of the actions
             layer_sizes ([int, ...., int]): number of hidden units for each layer
         """
+        print("Initializing model")
         self.o_tf = inputs_tf['o']
         self.u_tf = inputs_tf['u']
-
+        #
         dimo = self.o_tf.shape[1]
-
+        #
         hidden = 100
         layers = 3
-
+        #
         with tf.variable_scope('ModelRNN'):
             input = tf.concat(axis=1, values=[self.o_tf, self.u_tf])
             self.output = nn(input, [hidden] * layers + [dimo])
