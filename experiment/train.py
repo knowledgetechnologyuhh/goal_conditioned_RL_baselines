d57ba90f2e5d29d4950abcbd660778a0f6b7d250
eppe <eppe@informatik.uni-hamburg.de>
2018-10-18 14:11:52 +0200

    Fixed creation of multiple diorectories when using num_cpu > 1

--- experiment/train.py
+++ experiment/train.py
@@ -223,10 +223,10 @@ def main(ctx, **kwargs):
             logdir = os.path.join(kwargs['base_logdir'], kwargs['env'], param_subdir)
         subdir_exists = os.path.exists(logdir)
         ctr += 1
-    print("Data dir: {}".format(logdir))
-    os.makedirs(logdir, exist_ok=False)
-
-    time.sleep(10)
+    # print("Data dir: {}".format(logdir))
+    # os.makedirs(logdir, exist_ok=False)
+    #
+    # time.sleep(10)
 
     kwargs['logdir'] = logdir
     kwargs['seed'] = int(time.time())
