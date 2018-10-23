ca0c0c4498f4e7062c13961623ff8a8e02ae9b91
eppe <eppe@informatik.uni-hamburg.de>
2018-10-23 17:09:45 +0200

    Running version to test and compare different model networks with random actions and acceptable visualization of learning progress

--- baselines/model_based/model_rnn.py
+++ baselines/model_based/model_rnn.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 from baselines.util import store_args, nn
-from tensorflow.contrib.rnn import GRUCell, LSTMCell
+from tensorflow.contrib.rnn import GRUCell, LSTMCell, MultiRNNCell
 import numpy as np
 
 # For resetable/resumable LSTMs look here:
@@ -48,6 +48,48 @@ class State_GRU1:
             self.output = tf.layers.dense(out, dimo)
 
         self.obs_loss_tf = tf.reduce_mean(tf.abs(self.output - self.o2_tf))
+
+class State_GRU2:
+    @store_args
+    def __init__(self, inputs_tf, **kwargs):
+        """The actor-critic network and related training code.
+
+        Args:
+            inputs_tf (dict of tensors): all necessary inputs for the network: the
+                observation (o), the action (u) and the successive observation (o2) (o2 not required for the network)
+        """
+        print("Initializing model")
+        self.o_tf = inputs_tf['o']
+        self.o2_tf = inputs_tf['o2']
+        self.u_tf = inputs_tf['u']
+        self.max_batch_size = kwargs['model_train_batch_size']
+
+        #
+        dimo = self.o_tf.shape[2]
+        #
+        sizes = [100,100]
+
+        # layers = 3
+        with tf.variable_scope('ModelRNN'):
+            # create a BasicRNNCell
+            self.rnn_cell = MultiRNNCell([tf.nn.rnn_cell.GRUCell(size) for size in sizes])
+
+            # defining initial zero state
+            self.initial_state = self.rnn_cell.zero_state(self.max_batch_size, dtype=tf.float32)
+
+            input = tf.concat(axis=2, values=[self.o_tf, self.u_tf])
+
+            out, state = tf.nn.dynamic_rnn(
+                self.rnn_cell,
+                input,
+                initial_state=self.initial_state,
+                dtype=tf.float32)
+
+            self.state = state
+            self.output = tf.layers.dense(out, dimo)
+
+        self.obs_loss_tf = tf.reduce_mean(tf.abs(self.output - self.o2_tf))
+
 
 class GRU_Simple:
     @store_args
@@ -117,7 +159,7 @@ class LSTM_Simple:
         self.obs_loss_tf = tf.reduce_mean(tf.square(self.output - self.o2_tf))
 
 
-class FF_Simple:
+class FF_Simple3:
     @store_args
     def __init__(self, inputs_tf, **kwargs):
         """The actor-critic network and related training code.
@@ -135,6 +177,31 @@ class FF_Simple:
         #
         hidden = 100
         layers = 3
+        #
+        with tf.variable_scope('ModelRNN'):
+            input = tf.concat(axis=2, values=[self.o_tf, self.u_tf])
+            self.output = nn(input, [hidden] * layers + [dimo])
+
+        self.obs_loss_tf = tf.reduce_mean(tf.square(self.output - self.o2_tf))
+
+class FF_Simple5:
+    @store_args
+    def __init__(self, inputs_tf, **kwargs):
+        """The actor-critic network and related training code.
+
+        Args:
+            inputs_tf (dict of tensors): all necessary inputs for the network: the
+                observation (o), the action (u) and the successive observation (o2) (o2 not required for the network)
+        """
+        print("Initializing model")
+        self.o_tf = inputs_tf['o']
+        self.o2_tf = inputs_tf['o2']
+        self.u_tf = inputs_tf['u']
+        #
+        dimo = self.o_tf.shape[2]
+        #
+        hidden = 100
+        layers = 5
         #
         with tf.variable_scope('ModelRNN'):
             input = tf.concat(axis=2, values=[self.o_tf, self.u_tf])
--- plot/plot.py
+++ plot/plot.py
@@ -61,12 +61,12 @@ def pad(xs, value=np.nan):
     return np.array(padded_xs)
 
 
-def draw_var_param_plots(data,var_param_keys,inter_dict, fig_dir):
+def draw_var_param_plots(data, var_param_keys, inter_dict, fig_dir,  y_axis_title=None):
     success_table = {}
     for config_split in var_param_keys:
         # config_split = 'mask_at_observation'
         for conf_val in inter_dict[config_split]:
-            print('processing {}: {}'.format(config_split, conf_val))
+            # print('processing {}: {}'.format(config_split, conf_val))
             # n_fig = 0
             plt.clf()
             # plt.figure(figsize=(20, 8))
@@ -80,14 +80,10 @@ def draw_var_param_plots(data,var_param_keys,inter_dict, fig_dir):
                 label = config + "-n: {}".format(len(data[config]))
                 if "{}: {}-".format(config_split, conf_val) not in label:
                     continue
-                print('processing {}'.format(config))
+                # print('processing {}'.format(config))
 
-                try:
-                    xs, ys = zip(*data[config])
-                except:
-                    continue
-
-                xs, ys = pad(xs), pad(ys, value=1.0)
+                xs, ys = zip(*data[config])
+                xs, ys = pad(xs), pad(ys, value=np.nan)
                 median = np.nanmedian(ys, axis=0)
                 mean = np.mean(ys, axis=0)
                 assert xs.shape == ys.shape
@@ -96,23 +92,13 @@ def draw_var_param_plots(data,var_param_keys,inter_dict, fig_dir):
                 plt.plot(x_vals, median, label=label)
                 plt.fill_between(x_vals, np.nanpercentile(ys, 25, axis=0), np.nanpercentile(ys, 75, axis=0), alpha=0.25)
 
-                final_success_rate = ys[-1]
-                reached_50_at = (np.abs(ys - 0.5)).argmin()
-                reached_75_at = (np.abs(ys - 0.75)).argmin()
-                reached_100_at = (np.abs(ys - 1.0)).argmin()
-
-                # plt.plot(x_vals, mean, label=label+"-mean")
-                # dev = np.std(ys, axis=0)
-                # min_vals=mean-dev
-                # max_vals = mean + dev
-                # plt.fill_between(x_vals, min_vals, max_vals, alpha=0.25)
             plt.xlabel('Epoch')
-            plt.ylabel('Success Rate')
+            plt.ylabel(y_axis_title)
             plt.legend()
-            plt.savefig(os.path.join(fig_dir, 'fig-{}:{}.png'.format(config_split, conf_val)))
+            plt.savefig(os.path.join(fig_dir, 'fig-{}:{}_{}.png'.format(config_split, conf_val, y_axis_title.replace("/", "_"))))
 
 
-def draw_all_data_plot(data, fig_dir):
+def draw_all_data_plot(data, fig_dir, y_axis_title=None):
     plt.clf()
     # plt.figure(figsize=(20, 8))
     fig = plt.figure(figsize=(20, 8))
@@ -122,8 +108,6 @@ def draw_all_data_plot(data, fig_dir):
                   '#bcbd22', '#17becf']
     plt.rc('axes', prop_cycle=(cycler('linestyle', ['-', '--', ':']) * cycler('color', new_colors)))
     for idx, config in enumerate(sorted(data.keys(), reverse=True)):
-        label = config + " - n: {}".format(len(data[config]))
-
         label = "+".join(sorted(config.split("-"), reverse=True))
 
         # Some custom modifications of label:
@@ -134,16 +118,18 @@ def draw_all_data_plot(data, fig_dir):
         label = label.replace("+goldilocks_sampling: none", '')
         if 'stochastic3_' in label:
             rg = label.split("stochastic3_")[1].split("_")[0]
-            kappa= label.split("stochastic3_")[1].split("_")[2]
+            kappa = label.split("stochastic3_")[1].split("_")[2]
             h = label.split("stochastic3_")[1].split("_")[3].split("+")[0]
             gl_str = "goldilocks_sampling: stochastic3_{}_0_{}_{}".format(rg,kappa,h)
             # label = label.replace("stochastic3_{}_0_{}_{}".format(rg,c,h), 'CGM \nrg={} \nc={}'.format(rg,kappa))
             label = label.replace(gl_str, "CGM")
-
+        label = label.replace("model_network_class: ", 'model: ')
+        label = label.replace("baselines.model_based.model_rnn:", '')
+        # label = label.replace("+goldilocks_sampling: none", '')
         # End custom modifications of label
 
         xs, ys = zip(*data[config])
-        xs, ys = pad(xs), pad(ys, value=1.0)
+        xs, ys = pad(xs), pad(ys, value=np.nan)
         median = np.nanmedian(ys, axis=0)
         mean = np.mean(ys, axis=0)
         assert xs.shape == ys.shape
@@ -164,15 +150,15 @@ def draw_all_data_plot(data, fig_dir):
         # max_vals = mean + dev
         # plt.fill_between(x_vals, min_vals, max_vals, alpha=0.25)
     # plt.title(env_id)
-    ax.set_xlim([0, 150])
+    # ax.set_xlim([0, 150])
     plt.xlabel('epoch')
-    plt.ylabel('success rate')
-    # plt.legend(loc='upper left', bbox_to_anchor=(0.1,0.9))
+    plt.ylabel(y_axis_title)
+    plt.legend(loc='upper right')
     fig.tight_layout()
-    plt.savefig(os.path.join(fig_dir, 'fig.png'))
+    plt.savefig(os.path.join(fig_dir, 'fig_{}.png'.format(y_axis_title.replace("/", "_"))))
 
 
-def draw_all_data_plot_rg_c_conv(data, fig_dir):
+def draw_all_data_plot_rg_c_conv(data, fig_dir, y_axis_title=None):
     plt.clf()
     # plt.figure(figsize=(20, 8))
     fig = plt.figure(figsize=(10, 5))
@@ -222,7 +208,7 @@ def draw_all_data_plot_rg_c_conv(data, fig_dir):
     # plt.title(env_id)
     ax.set_xlim([4, 15])
     plt.xlabel('epoch')
-    plt.ylabel('success rate')
+    plt.ylabel(y_axis_title)
     plt.legend(loc='upper left')
     # fig.tight_layout()
     plt.savefig(os.path.join(fig_dir, 'fig.png'))
@@ -358,17 +344,7 @@ def draw_stochastic_surface_plot(data, percent_to_achieve, fig_dir):
     plt.savefig(os.path.join(fig_dir, 'c_rg_.png'))
 
 
-def main():
-    matplotlib.rcParams['font.family'] = "serif"
-    matplotlib.rcParams['font.weight'] = 'normal'
-    parser = argparse.ArgumentParser()
-    parser.add_argument('dir', type=str)
-    parser.add_argument('--smooth', type=int, default=0)
-    args = parser.parse_args()
-
-    # Load all data.
-    data = {}
-    paths = [os.path.abspath(os.path.join(path, '..')) for path in glob2.glob(os.path.join(args.dir, '**', 'progress.csv'))]
+def get_var_param_keys(paths):
     # all_params = []
     inter_dict = {}
     var_param_keys = set()
@@ -376,42 +352,40 @@ def main():
     for curr_path in paths:
         if not os.path.isdir(curr_path):
             continue
-        print('loading {}'.format(curr_path))
+        # print('loading {}'.format(curr_path))
         results = load_results(os.path.join(curr_path, 'progress.csv'))
         if not results:
-            print('skipping {}'.format(curr_path))
+            print('skipping plot for {}'.format(curr_path))
             continue
-        if len(results) != 10:
-            print('skipping {}, bad file format'.format(curr_path))
-            continue
+        # if len(results) != 10:
+        #     print('skipping {}, bad file format'.format(curr_path))
+        #     continue
         # print('loading {} ({})'.format(curr_path, len(results['epoch'])))
         with open(os.path.join(curr_path, 'params.json'), 'r') as f:
             params = json.load(f)
-        for k,v in params.items():
+        for k, v in params.items():
             if k not in inter_dict.keys():
                 inter_dict[k] = [v]
             if v not in inter_dict[k]:
                 inter_dict[k].append(v)
                 var_param_keys.add(k)
         max_epochs = max(max_epochs, len(results['epoch']))
+    return var_param_keys, inter_dict, max_epochs
 
+def get_data(paths, var_param_keys, max_epochs, smoothen=False, padding=True, col_to_display='test/success_rate', data_lastval_threshold=0.0):
+    data = {}
     for curr_path in paths:
         if not os.path.isdir(curr_path):
             continue
-        print('loading {}'.format(curr_path))
+        # print('loading {}'.format(curr_path))
         results = load_results(os.path.join(curr_path, 'progress.csv'))
         if not results:
-            print('skipping {}'.format(curr_path))
+            print('skipping plot for {}'.format(curr_path))
             continue
-        if len(results) != 10:
-            print('skipping {}, bad file format'.format(curr_path))
-            continue
-        # if float(curr_path.split("_")[-1]) > 20:
-        #     continue
-        print('loading {} ({})'.format(curr_path, len(results['epoch'])))
+        # print('loading {} ({})'.format(curr_path, len(results['epoch'])))
         with open(os.path.join(curr_path, 'params.json'), 'r') as f:
             params = json.load(f)
-        success_rate = np.array(results['test/success_rate'])
+        this_data = np.array(results[col_to_display])
 
         epoch = np.array(results['epoch']) + 1
         if len(epoch) < 3:
@@ -426,31 +400,54 @@ def main():
         config = config[:-1]
 
         # Process and smooth data.
-        assert success_rate.shape == epoch.shape
+        assert this_data.shape == epoch.shape
         x = epoch
-        x = np.array(range(1, max_epochs+1))
-        y = np.array(success_rate)
-        pad_val = y[-1]
-        y_pad = np.array([pad_val] * (max_epochs - len(y)))
-        y = np.concatenate((y,y_pad))
+        y = np.array(this_data)
+        if padding:
+            x = np.array(range(1, max_epochs+1))
+            pad_val = y[-1]
+            y_pad = np.array([pad_val] * (max_epochs - len(y)))
+            y = np.concatenate((y,y_pad))
 
-        if args.smooth:
-            x, y = smooth_reward_curve(epoch, success_rate)
+        if smoothen:
+            x, y = smooth_reward_curve(epoch, this_data)
         if x.shape != y.shape:
             continue
 
-        if y[-1] > success_threshold or (len(epoch) == max_epochs):
+        if y[-1] >= data_lastval_threshold or (len(epoch) == max_epochs):
             if config not in data.keys():
                 data[config] = []
             data[config].append((x, y))
 
-    # draw_var_param_plots(data, var_param_keys, inter_dict, args.dir)
-    draw_all_data_plot(data, args.dir)
+    return data
+
+
+def do_plot(data_dir, smoothen=True, padding=False, col_to_display='test/success_rate'):
+    matplotlib.rcParams['font.family'] = "serif"
+    matplotlib.rcParams['font.weight'] = 'normal'
+
+
+    paths = [os.path.abspath(os.path.join(path, '..')) for path in glob2.glob(os.path.join(data_dir, '**', 'progress.csv'))]
+
+    var_param_keys, inter_dict, max_epochs = get_var_param_keys(paths)
+
+    data = get_data(paths, var_param_keys, max_epochs, smoothen, padding, col_to_display=col_to_display)
+
+    # draw_var_param_plots(data, var_param_keys, inter_dict, data_dir, y_axis_title=col_to_display)
+
+    draw_all_data_plot(data, data_dir, y_axis_title=col_to_display)
+
     # draw_all_data_plot_rg_c_conv(data, args.dir)
     # rate_to_achieve = 0.5
     # draw_stochastic_surface_plot(data, rate_to_achieve, args.dir)
 
 
 if __name__ == '__main__':
-    success_threshold = 0.0
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('data_dir', type=str)
+    parser.add_argument('--smooth', type=int, default=0)
+    parser.add_argument('--pad', type=int, default=0)
+    parser.add_argument('--column', type=str, default='test/success_rate')
+    args = parser.parse_args()
+    data_lastval_threshold = 0.0
+    do_plot(args.data_dir, args.smooth, args.pad, col_to_display=args.column, data_lastval_threshold=data_lastval_threshold)
