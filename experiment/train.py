4f9bfdf0856ad8875c0f11f9fb27f468e9420051
eppe <eppe@informatik.uni-hamburg.de>
2018-10-29 16:56:33 +0100

    Added adaptive model learning rate

--- experiment/train.py
+++ experiment/train.py
@@ -204,7 +204,8 @@ def main(ctx, **kwargs):
     global config, RolloutWorker, policy_linker
     config, RolloutWorker = main_linker.import_creator(kwargs['algorithm'])
     policy_args = ctx.forward(main_linker.get_policy_click)
-    policy_args.update({ctx.args[i][2:]: ctx.args[i + 1] for i in range(0, len(ctx.args), 2)})
+    cmd_line_update_args = {ctx.args[i][2:]: type(policy_args[ctx.args[i][2:]])(ctx.args[i + 1]) for i in range(0, len(ctx.args), 2)}
+    policy_args.update(cmd_line_update_args)
     kwargs.update(policy_args)
 
     override_params = config.OVERRIDE_PARAMS_LIST
