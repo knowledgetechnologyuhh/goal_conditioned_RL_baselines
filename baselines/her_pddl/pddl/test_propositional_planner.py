35387ccb664df7260276ee1387906c315806477b
eppe <eppe@informatik.uni-hamburg.de>
2019-02-06 17:01:37 +0100

    her_ppdl: pddl obs_to_preds working for one object.

---
+++ baselines/her_pddl/pddl/test_propositional_planner.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# Four spaces as indentation [no tabs]
+
+import unittest
+from pddl.action import Action
+from pddl.propositional_planner import Propositional_Planner
+
+# ==========================================
+# Test Propositional_Planner
+# ==========================================
+
+class Test_Propositional_Planner(unittest.TestCase):
+
+    # ------------------------------------------
+    # Test solve
+    # ------------------------------------------
+
+    def test_solve_dinner(self):
+        planner = Propositional_Planner()
+        self.assertEqual(planner.solve('dinner/dinner.pddl', 'dinner/pb1.pddl'),
+            [
+                Action('cook', [], [['clean']], [], [['dinner']], []),
+                Action('wrap', [], [['quiet']], [], [['present']], []),
+                Action('carry', [], [['garbage']], [], [], [['garbage'], ['clean']])
+            ]
+        )
+
+    #-------------------------------------------
+    # Split propositions
+    #-------------------------------------------
+
+if __name__ == '__main__':
+    unittest.main()
