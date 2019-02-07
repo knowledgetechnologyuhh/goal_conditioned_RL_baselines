1df5b51e40af82ce7510735a6e09e7e27939e94e
eppe <eppe@informatik.uni-hamburg.de>
2019-02-07 12:34:45 +0100

    Subgoals seem to be working

--- baselines/her_pddl/interface/config.py
+++ baselines/her_pddl/interface/config.py
@@ -40,10 +40,10 @@ DEFAULT_PARAMS = {
     'n_test_rollouts': 10,  # number of test rollouts per epoch, each consists of rollout_batch_size rollouts
     'test_with_polyak': False,  # run test episodes with the target network
     # exploration
-    # 'random_eps': 0.3,  # percentage of time a random action is taken
-    # 'noise_eps': 0.2,  # std of gaussian noise added to not-completely-random actions as a percentage of max_u
-    'random_eps': 0.05,  # percentage of time a random action is taken
-    'noise_eps': 0.05,  # std of gaussian noise added to not-completely-random actions as a percentage of max_u
+    'random_eps': 0.3,  # percentage of time a random action is taken
+    'noise_eps': 0.2,  # std of gaussian noise added to not-completely-random actions as a percentage of max_u
+    # 'random_eps': 0.05,  # percentage of time a random action is taken
+    # 'noise_eps': 0.05,  # std of gaussian noise added to not-completely-random actions as a percentage of max_u
     # HER
     'replay_strategy': 'future',  # supported modes: future, none
     'replay_k': 4,  # number of additional goals used for replay, only used if off_policy_data=future
