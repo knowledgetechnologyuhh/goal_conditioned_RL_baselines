02045d043389e860d0948931c4c10b314079aa38
Phuong Nguyen <pnguyen@informatik.uni-hamburg.de>
2019-02-19 16:10:39 +0100

    [herhrl] make click_options readable

--- baselines/herhrl/interface/click_options.py
+++ baselines/herhrl/interface/click_options.py
@@ -2,9 +2,12 @@ import click
 _her_options = [
 click.option('--n_test_rollouts', type=int, default=10, help='The number of testing rollouts.'),
 click.option('--clip_return', type=int, default=1, help='whether or not returns should be clipped'),
-click.option('--network_class', type=str, default='baselines.her.actor_critic:ActorCritic', help='The Neural network model to use.'),
-click.option('--replay_k', type=int, default=4, help='The ratio between HER replays and regular replays. Set to 0 for DDPG only.'),
-click.option('--train_batch_size', type=int, default=256, help='The number of state transitions processed during network training.'),
+click.option('--network_class', type=str, default='baselines.herhrl.actor_critic:ActorCritic',
+             help='The Neural network model to use.'),
+click.option('--replay_k', type=int, default=4,
+             help='The ratio between HER replays and regular replays. Set to 0 for DDPG only.'),
+click.option('--train_batch_size', type=int, default=256,
+             help='The number of state transitions processed during network training.'),
 click.option('--n_train_batches', type=int, default=40, help='The number of batches for model training.'),
 ]
 
