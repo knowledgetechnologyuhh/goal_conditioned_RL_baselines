52345a29e39776ae035eb24f4c8438a4477a96ee
eppe <eppe@informatik.uni-hamburg.de>
2019-02-19 18:00:06 +0100

    Now performing in_memory plan caching.

--- wtm_envs/mujoco/tower_env_pddl.py
+++ wtm_envs/mujoco/tower_env_pddl.py
@@ -285,7 +285,7 @@ def gen_plan_single(preds, gripper_has_target, goal_preds, ignore_actions=[]):
     plan_start = time.time()
     plan = planner.solve(domain, problem)
     duration = time.time() - plan_start
-    if duration > 0.5:
+    if duration > 0.0:
         print("plan generation took {:.2f} sec.".format(time.time() - plan_start))
 
     plan_acts = []
