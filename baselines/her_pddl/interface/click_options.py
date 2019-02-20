8249dfa2814e742dee8a9c4d2f5e9053a15877f4
eppe <eppe@informatik.uni-hamburg.de>
2019-02-20 17:50:38 +0100

    Parameterizable representation network class

--- baselines/her_pddl/interface/click_options.py
+++ baselines/her_pddl/interface/click_options.py
@@ -2,7 +2,8 @@ import click
 _her_options = [
 click.option('--n_test_rollouts', type=int, default=10, help='The number of testing rollouts.'),
 click.option('--clip_return', type=int, default=1, help='whether or not returns should be clipped'),
-click.option('--network_class', type=str, default='baselines.her.actor_critic:ActorCritic', help='The Neural network model to use.'),
+click.option('--network_class', type=str, default='baselines.her_pddl.actor_critic:ActorCritic', help='The Neural network model to use for RL.'),
+click.option('--rep_network_class', type=str, default='baselines.her_pddl.obs2preds:Obs2PredsDenseModel', help='The Neural network model to use for learning representations.'),
 click.option('--replay_k', type=int, default=4, help='The ratio between HER replays and regular replays. Set to 0 for DDPG only.'),
 click.option('--train_batch_size', type=int, default=256, help='The number of state transitions processed during network training.'),
 click.option('--n_train_batches', type=int, default=40, help='The number of batches for model training.'),
