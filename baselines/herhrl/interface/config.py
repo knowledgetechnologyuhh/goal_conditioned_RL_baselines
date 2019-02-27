4898e2b07e058b265fe95e579e40f604b7ead88d
Phuong Nguyen <pnguyen@informatik.uni-hamburg.de>
2019-02-27 12:53:58 +0100

    [herhrl-config] use different sample_transition for child_rollout, change number of layers

--- baselines/herhrl/interface/config.py
+++ baselines/herhrl/interface/config.py
@@ -20,7 +20,7 @@ DEFAULT_PARAMS = {
     # env
     'max_u': 1.,  # max absolute value of actions on different coordinates
     # ddpg
-    'layers': 3,  # number of layers in the critic/actor networks
+    'layers': 5,  # number of layers in the critic/actor networks
     'hidden': 256,  # number of neurons in each hidden layers
     'network_class': 'baselines.herhrl.actor_critic:ActorCritic',
     'Q_lr': 0.001,  # critic learning rate
@@ -197,7 +197,7 @@ def configure_policy(dims, params):
                         'clip_return': (1. / (1. - gamma)) if params['clip_return'] else np.inf,  # max abs of return
                         'rollout_batch_size': rollout_batch_size,
                         'subtract_goals': simple_goal_subtract,
-                        'sample_transitions': sample_her_transitions_child,
+                        #'sample_transitions': sample_her_transitions_child,
                         'gamma': gamma,
                         'reuse': reuse,
                         'use_mpi': use_mpi,
@@ -207,8 +207,11 @@ def configure_policy(dims, params):
     ddpg_params['info'] = {
         'env_name': params['env_name'],
     }
+    ddpg_params['sample_transitions'] = sample_her_transitions_child
     ddpg_params['T'] = n_subgoals
     policy_child = DDPG(**ddpg_params)
+
+    ddpg_params['max_u'] = .5
     ddpg_params['scope'] += '_parent'
     ddpg_params['T'] = params['T']
     ddpg_params['sample_transitions'] = sample_her_transitions
