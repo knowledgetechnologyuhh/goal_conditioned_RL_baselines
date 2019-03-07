ff139bac19b126d32ab6a675b97cd7705dfe59d9
eppe <eppe@informatik.uni-hamburg.de>
2019-03-07 17:03:02 +0100

    Ready to do some testing and comparison with HER.

--- set_paths.sh
+++ set_paths.sh
@@ -1,3 +1,5 @@
+nv_version=$(modinfo $(find /lib/modules/$(uname -r) -iname nvidia_*.ko | head -1) | grep ^version: | cut -d':' -f 2 | cut -d'.' -f 1)
+echo $nv_version
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/informatik3/wtm/home/eppe/.mujoco/mjpro150/bin
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/nvidia-410
-export LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libGLEW.so:/usr/lib/nvidia-410/libGL.so
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/nvidia-{$nv_version}
+export LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libGLEW.so:/usr/lib/nvidia-{$nv_version}/libGL.so
