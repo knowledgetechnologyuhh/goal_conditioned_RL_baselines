8c5b591931ae33ed593ea3c7083fdc7b31493cdc
eppe <eppe@informatik.uni-hamburg.de>
2019-03-18 14:41:50 +0100

    Fixed initial_o and initial_ag (not needed, i guess)

--- run_hyperopt_herhrl.sh
+++ run_hyperopt_herhrl.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
-#export LD_LIBRARY_PATH=/usr/local/cuda/lib:/usr/local/cuda/lib64:/usr/local/cudnn/lib64:/informatik3/wtm/home/eppe/.mujoco/mjpro150/bin:/usr/lib/nvidia-387
-#export LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libGLEW.so:/usr/lib/nvidia-384/libGL.so
 source ./set_paths.sh
 
 n_cpu=16
@@ -10,17 +8,7 @@ n_epochs=150
 n_objects=1
 min_th=1
 max_th=1
-#buff_sampling='random'
-#memval_method='uniform'
-#action_selection='random'
 
-
-
-#n_train_batches=$1
-#model_lr=$2
-#action_selection=$3
-#memval_method=$4
-#buff_sampling=$5
 penalty_magnitude=$1
 test_subgoal_perc=$2
 
