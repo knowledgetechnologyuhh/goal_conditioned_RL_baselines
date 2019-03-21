54114f35b57d89c7831d5ad74e43d1db2c09b391
eppe <eppe@informatik.uni-hamburg.de>
2019-03-21 08:46:01 +0100

    Added script for running best hyperopt parameterizations multiple times

--- run_hyperopt_final.sh
+++ run_hyperopt_final.sh
@@ -22,22 +22,38 @@ for i in 1 2 3
 do
     penalty_magnitude='10'
     test_subgoal_perc='0.1'
+    cmd="python3 experiment/train.py --num_cpu ${n_cpu} --env ${env} --algorithm baselines.herhrl
+--rollout_batch_size ${rollout_batch_size} --n_epochs ${n_epochs} --n_episodes ${n_episodes}
+--base_logdir /data/$(whoami)/herhrl --render 0 --penalty_magnitude ${penalty_magnitude}
+--test_subgoal_perc ${test_subgoal_perc}"
     echo ${cmd}
     ${cmd}
 
     penalty_magnitude='30'
     test_subgoal_perc='0.5'
+    cmd="python3 experiment/train.py --num_cpu ${n_cpu} --env ${env} --algorithm baselines.herhrl
+--rollout_batch_size ${rollout_batch_size} --n_epochs ${n_epochs} --n_episodes ${n_episodes}
+--base_logdir /data/$(whoami)/herhrl --render 0 --penalty_magnitude ${penalty_magnitude}
+--test_subgoal_perc ${test_subgoal_perc}"
     echo ${cmd}
     ${cmd}
 
 
     penalty_magnitude='20'
     test_subgoal_perc='0.2'
+    cmd="python3 experiment/train.py --num_cpu ${n_cpu} --env ${env} --algorithm baselines.herhrl
+--rollout_batch_size ${rollout_batch_size} --n_epochs ${n_epochs} --n_episodes ${n_episodes}
+--base_logdir /data/$(whoami)/herhrl --render 0 --penalty_magnitude ${penalty_magnitude}
+--test_subgoal_perc ${test_subgoal_perc}"
     echo ${cmd}
     ${cmd}
 
     penalty_magnitude='10'
     test_subgoal_perc='1.0'
+    cmd="python3 experiment/train.py --num_cpu ${n_cpu} --env ${env} --algorithm baselines.herhrl
+--rollout_batch_size ${rollout_batch_size} --n_epochs ${n_epochs} --n_episodes ${n_episodes}
+--base_logdir /data/$(whoami)/herhrl --render 0 --penalty_magnitude ${penalty_magnitude}
+--test_subgoal_perc ${test_subgoal_perc}"
     echo ${cmd}
     ${cmd}
 done
