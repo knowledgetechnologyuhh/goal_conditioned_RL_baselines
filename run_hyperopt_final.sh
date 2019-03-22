62b7e8c5e6cfa0592465198bfa20a1394474f89f
eppe <eppe@informatik.uni-hamburg.de>
2019-03-22 14:55:07 +0100

    Ready to do weekend run!

--- run_hyperopt_final.sh
+++ run_hyperopt_final.sh
@@ -12,12 +12,6 @@ max_th=1
 krenew -K 60 -b
 env="TowerBuildMujocoEnv-sparse-gripper_random-o${n_objects}-h${min_th}-${max_th}-v1"
 
-cmd="python3 experiment/train.py --num_cpu ${n_cpu} --env ${env} --algorithm baselines.herhrl
---rollout_batch_size ${rollout_batch_size} --n_epochs ${n_epochs} --n_episodes ${n_episodes}
---base_logdir /data/$(whoami)/herhrl --render 0 --penalty_magnitude ${penalty_magnitude}
---test_subgoal_perc ${test_subgoal_perc}"
-
-
 for i in 1 2 3 4 5
 do
     penalty_magnitude='20'
