9abee29ce5c47501575edc9181f41be8b4044e9c
eppe <eppe@informatik.uni-hamburg.de>
2019-03-29 15:23:09 +0100

    Getting ready for weekend run

--- run_debug_test.sh
+++ run_debug_test.sh
@@ -1,6 +1,6 @@
-python3 experiment/train.py --n_episodes 10 --n_test_rollouts 3 --env TowerBuildMujocoEnv-sparse-gripper_random-o2-h1-1-v1 --algorithm baselines.herhrl --render 0 --num_cpu 2 --rollout_batch_size 2 --n_epochs 3 --n_subgoals_layers [] --policies_layers []
-
-python3 experiment/train.py --n_episodes 10 --n_test_rollouts 3 --env TowerBuildMujocoEnv-sparse-gripper_random-o2-h1-1-v1 --algorithm baselines.herhrl --render 0 --num_cpu 2 --rollout_batch_size 2 --n_epochs 3 --n_subgoals_layers [7] --policies_layers [DDPG_HER_HRL_POLICY]
+#python3 experiment/train.py --n_episodes 10 --n_test_rollouts 3 --env TowerBuildMujocoEnv-sparse-gripper_random-o2-h1-1-v1 --algorithm baselines.herhrl --render 0 --num_cpu 2 --rollout_batch_size 2 --n_epochs 3 --n_subgoals_layers [] --policies_layers []
+#
+#python3 experiment/train.py --n_episodes 10 --n_test_rollouts 3 --env TowerBuildMujocoEnv-sparse-gripper_random-o2-h1-1-v1 --algorithm baselines.herhrl --render 0 --num_cpu 2 --rollout_batch_size 2 --n_epochs 3 --n_subgoals_layers [7] --policies_layers [DDPG_HER_HRL_POLICY]
 
 python3 experiment/train.py --n_episodes 10 --n_test_rollouts 3 --env TowerBuildMujocoEnv-sparse-gripper_random-o2-h1-1-v1 --algorithm baselines.herhrl --render 0 --num_cpu 2 --rollout_batch_size 2 --n_epochs 3 --n_subgoals_layers [7] --policies_layers [PDDL_POLICY]
 
