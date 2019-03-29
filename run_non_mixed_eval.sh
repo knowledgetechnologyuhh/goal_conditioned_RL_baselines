bce4d9ba7628d034c9f69190bfb35fc70d3c01b3
eppe <eppe@informatik.uni-hamburg.de>
2019-03-29 15:53:05 +0100

    Getting ready for weekend run

--- run_non_mixed_eval.sh
+++ run_non_mixed_eval.sh
@@ -17,10 +17,7 @@ max_th=1
 krenew -K 60 -b
 env="TowerBuildMujocoEnv-sparse-gripper_random-o${n_objects}-h${min_th}-${max_th}-v1"
 
-
-for i in 1 2 3 4 5
-do
-    cmd="python3 experiment/train.py --num_cpu ${n_cpu} --env ${env} --algorithm baselines.herhrl
+cmd="python3 experiment/train.py --num_cpu ${n_cpu} --env ${env} --algorithm baselines.herhrl
         --rollout_batch_size ${rollout_batch_size} --n_epochs ${n_epochs} --n_episodes ${n_episodes}
         --base_logdir /data/$(whoami)/herhrl --render 0 --policies_layers [] --n_subgoals_layers []"
             echo ${cmd}
@@ -37,20 +34,26 @@ do
         --base_logdir /data/$(whoami)/herhrl --render 0 --policies_layers [DDPG_HER_HRL_POLICY] --n_subgoals_layers [3]"
             echo ${cmd}
             ${cmd}
-#    for p_steepness in '2.0' '0.5' '1.0' '4.0'
-#    do
-#        for p_threshold in '0.2' '0.1' '0.05'
-#        do
-#            cmd="python3 experiment/train.py --num_cpu ${n_cpu} --env ${env} --algorithm baselines.herhrl
+
+#for i in 1 2 3 4 5
+#do
+#    cmd="python3 experiment/train.py --num_cpu ${n_cpu} --env ${env} --algorithm baselines.herhrl
 #        --rollout_batch_size ${rollout_batch_size} --n_epochs ${n_epochs} --n_episodes ${n_episodes}
-#        --base_logdir /data/$(whoami)/herhrl --render 0 --penalty_magnitude ${penalty_magnitude}
-#        --test_subgoal_perc ${test_subgoal_perc} --policies_layers [] --mix_p_threshold ${p_threshold}
-#        --mix_p_steepness ${p_steepness}"
+#        --base_logdir /data/$(whoami)/herhrl --render 0 --policies_layers [] --n_subgoals_layers []"
 #            echo ${cmd}
 #            ${cmd}
 #
-#        done
-#    done
-done
-
-
+#    cmd="python3 experiment/train.py --num_cpu ${n_cpu} --env ${env} --algorithm baselines.herhrl
+#        --rollout_batch_size ${rollout_batch_size} --n_epochs ${n_epochs} --n_episodes ${n_episodes}
+#        --base_logdir /data/$(whoami)/herhrl --render 0 --policies_layers [PDDL_POLICY] --n_subgoals_layers [3]"
+#            echo ${cmd}
+#            ${cmd}
+#
+#    cmd="python3 experiment/train.py --num_cpu ${n_cpu} --env ${env} --algorithm baselines.herhrl
+#        --rollout_batch_size ${rollout_batch_size} --n_epochs ${n_epochs} --n_episodes ${n_episodes}
+#        --base_logdir /data/$(whoami)/herhrl --render 0 --policies_layers [DDPG_HER_HRL_POLICY] --n_subgoals_layers [3]"
+#            echo ${cmd}
+#            ${cmd}
+#done
+#
+#
