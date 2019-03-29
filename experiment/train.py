42cdbf785012cf7d8cbfb4561c2477017eec4f34
eppe <eppe@informatik.uni-hamburg.de>
2019-03-29 07:56:12 +0100

    Trying to fix mixed policy issue in mpi_moments.py

--- experiment/train.py
+++ experiment/train.py
@@ -61,26 +61,29 @@ def train(rollout_worker, evaluator,
         logger.record_tabular('epoch', epoch)
         for key, val in evaluator.logs('test'):
             # logger.record_tabular(key, mpi_average(val))
-            try:
-                logger.record_tabular(key, mpi_average(val))
-            except Exception as e:
-                logger.warn("Error with kv {}:{} -- {}".format(key,val, e))
-                logger.record_tabular(key, val)
+            logger.record_tabular(key, val)
+            # try:
+            #     logger.record_tabular(key, mpi_average(val))
+            # except Exception as e:
+            #     logger.warn("Error with kv {}:{} -- {}".format(key,val, e))
+            #     logger.record_tabular(key, val)
         for key, val in rollout_worker.logs('train'):
             # logger.record_tabular(key, mpi_average(val))
-            try:
-                logger.record_tabular(key, mpi_average(val))
-            except Exception as e:
-                logger.warn("Error with kv {}:{} -- {}".format(key, val, e))
-                logger.record_tabular(key, val)
+            logger.record_tabular(key, val)
+            # try:
+            #     logger.record_tabular(key, mpi_average(val))
+            # except Exception as e:
+            #     logger.warn("Error with kv {}:{} -- {}".format(key, val, e))
+            #     logger.record_tabular(key, val)
         for key, val in policy.logs('policy'):
             # logger.record_tabular(key, mpi_average(val))
-            try:
-                logger.record_tabular(key, mpi_average(val))
-            except Exception as e:
-            #     print("Error with kv {}:{} -- {}".format(key,val, e))
-                logger.warn("Error with kv {}:{} -- {}".format(key, val, e))
-                logger.record_tabular(key, val)
+            logger.record_tabular(key, val)
+            # try:
+            #     logger.record_tabular(key, mpi_average(val))
+            # except Exception as e:
+            # #     print("Error with kv {}:{} -- {}".format(key,val, e))
+            #     logger.warn("Error with kv {}:{} -- {}".format(key, val, e))
+            #     logger.record_tabular(key, val)
         if rank == 0:
             print("Data_dir: {}".format(logger.get_dir()))
             logger.dump_tabular()
