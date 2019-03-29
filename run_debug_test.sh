5865bc726a6a0d7e04a58f5a13bc44048771e1a6
eppe <eppe@informatik.uni-hamburg.de>
2019-03-29 15:15:28 +0100

    Trying to fix mixed policy issue in mpi_moments.py

--- run_debug_test.sh
+++ run_debug_test.sh
@@ -3,3 +3,7 @@ python3 experiment/train.py --n_episodes 10 --n_test_rollouts 3 --env TowerBuild
 python3 experiment/train.py --n_episodes 10 --n_test_rollouts 3 --env TowerBuildMujocoEnv-sparse-gripper_random-o2-h1-1-v1 --algorithm baselines.herhrl --render 0 --num_cpu 2 --rollout_batch_size 2 --n_epochs 3 --n_subgoals_layers [7] --policies_layers [DDPG_HER_HRL_POLICY]
 
 python3 experiment/train.py --n_episodes 10 --n_test_rollouts 3 --env TowerBuildMujocoEnv-sparse-gripper_random-o2-h1-1-v1 --algorithm baselines.herhrl --render 0 --num_cpu 2 --rollout_batch_size 2 --n_epochs 3 --n_subgoals_layers [7] --policies_layers [PDDL_POLICY]
+
+python3 experiment/train.py --n_episodes 10 --n_test_rollouts 3 --env TowerBuildMujocoEnv-sparse-gripper_random-o2-h1-1-v1 --algorithm baselines.herhrl --render 0 --num_cpu 2 --rollout_batch_size 2 --n_epochs 3 --n_subgoals_layers [7] --policies_layers [MIX_PDDL_HRL_POLICY]
+
+python3 experiment/train.py --n_episodes 10 --n_test_rollouts 3 --env TowerBuildMujocoEnv-sparse-gripper_random-o2-h1-1-v1 --algorithm baselines.her --render 0 --num_cpu 2 --rollout_batch_size 2 --n_epochs 3
