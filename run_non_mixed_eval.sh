3bed0c006b523f3e3816b13ba8b055244d90c78c
eppe <eppe@informatik.uni-hamburg.de>
2019-03-29 15:15:28 +0100

    Trying to fix mixed policy issue in mpi_moments.py

--- run_non_mixed_eval.sh
+++ run_non_mixed_eval.sh
@@ -22,19 +22,19 @@ for i in 1 2 3 4 5
 do
     cmd="python3 experiment/train.py --num_cpu ${n_cpu} --env ${env} --algorithm baselines.herhrl
         --rollout_batch_size ${rollout_batch_size} --n_epochs ${n_epochs} --n_episodes ${n_episodes}
-        --base_logdir /data/$(whoami)/herhrl --render 0 --policies_layers []"
+        --base_logdir /data/$(whoami)/herhrl --render 0 --policies_layers [] --n_subgoals_layers []"
             echo ${cmd}
             ${cmd}
 
     cmd="python3 experiment/train.py --num_cpu ${n_cpu} --env ${env} --algorithm baselines.herhrl
         --rollout_batch_size ${rollout_batch_size} --n_epochs ${n_epochs} --n_episodes ${n_episodes}
-        --base_logdir /data/$(whoami)/herhrl --render 0 --policies_layers [PDDL_POLICY]"
+        --base_logdir /data/$(whoami)/herhrl --render 0 --policies_layers [PDDL_POLICY] --n_subgoals_layers [3]"
             echo ${cmd}
             ${cmd}
 
     cmd="python3 experiment/train.py --num_cpu ${n_cpu} --env ${env} --algorithm baselines.herhrl
         --rollout_batch_size ${rollout_batch_size} --n_epochs ${n_epochs} --n_episodes ${n_episodes}
-        --base_logdir /data/$(whoami)/herhrl --render 0 --policies_layers [DDPG_HER_HRL_POLICY]"
+        --base_logdir /data/$(whoami)/herhrl --render 0 --policies_layers [DDPG_HER_HRL_POLICY] --n_subgoals_layers [3]"
             echo ${cmd}
             ${cmd}
 #    for p_steepness in '2.0' '0.5' '1.0' '4.0'
