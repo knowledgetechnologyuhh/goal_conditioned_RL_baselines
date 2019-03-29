5fb29eaae02de65a689950bac0dd9e0c007ae670
eppe <eppe@informatik.uni-hamburg.de>
2019-03-29 16:12:25 +0100

    Getting ready for weekend run

--- run_non_mixed_eval.sh
+++ run_non_mixed_eval.sh
@@ -20,7 +20,7 @@ env="TowerBuildMujocoEnv-sparse-gripper_random-o${n_objects}-h${min_th}-${max_th
 cmd="python3 experiment/train.py --num_cpu ${n_cpu} --env ${env} --algorithm baselines.herhrl
         --rollout_batch_size ${rollout_batch_size} --n_epochs ${n_epochs} --n_episodes ${n_episodes}
         --base_logdir /data/$(whoami)/herhrl --render 0 --policies_layers [] --n_subgoals_layers []"
-#            echo ${cmd}
+            echo ${cmd}
 #            ${cmd}
 
 #for i in 1 2
