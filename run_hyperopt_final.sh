b4bc9ede7ac734a513ebf01e67d646f888729555
eppe <eppe@informatik.uni-hamburg.de>
2019-03-29 14:37:20 +0100

    Trying to fix mixed policy issue in mpi_moments.py

--- run_hyperopt_final.sh
+++ run_hyperopt_final.sh
@@ -11,22 +11,27 @@ max_th=1
 penalty_magnitude=1
 test_subgoal_perc=0
 
+
 krenew -K 60 -b
 env="TowerBuildMujocoEnv-sparse-gripper_random-o${n_objects}-h${min_th}-${max_th}-v1"
 
 
-for i in 1 2 3
+for i in 1 2 3 4 5
 do
-    p_threshold='0.2'
-    p_steepness='2.0'
-    cmd="python3 experiment/train.py --num_cpu ${n_cpu} --env ${env} --algorithm baselines.herhrl
---rollout_batch_size ${rollout_batch_size} --n_epochs ${n_epochs} --n_episodes ${n_episodes}
---base_logdir /data/$(whoami)/herhrl --render 0 --penalty_magnitude ${penalty_magnitude}
---test_subgoal_perc ${test_subgoal_perc} --policies_layers [MIX_PDDL_HRL_POLICY] --mix_p_threshold ${p_threshold}
---mix_p_steepness ${p_steepness}"
-    echo ${cmd}
-    ${cmd}
+    for p_steepness in '2.0' '0.5' '1.0' '4.0'
+    do
+        for p_threshold in '0.2' '0.1' '0.05'
+        do
+            cmd="python3 experiment/train.py --num_cpu ${n_cpu} --env ${env} --algorithm baselines.herhrl
+        --rollout_batch_size ${rollout_batch_size} --n_epochs ${n_epochs} --n_episodes ${n_episodes}
+        --base_logdir /data/$(whoami)/herhrl --render 0 --penalty_magnitude ${penalty_magnitude}
+        --test_subgoal_perc ${test_subgoal_perc} --policies_layers [MIX_PDDL_HRL_POLICY] --mix_p_threshold ${p_threshold}
+        --mix_p_steepness ${p_steepness}"
+            echo ${cmd}
+            ${cmd}
 
+        done
+    done
 done
 
 
