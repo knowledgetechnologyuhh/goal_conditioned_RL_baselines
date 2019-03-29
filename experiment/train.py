5b5deb02b93ecc7e397a305262d2555118a76dd1
eppe <eppe@informatik.uni-hamburg.de>
2019-03-29 17:02:24 +0100

    Getting ready for weekend run

--- experiment/train.py
+++ experiment/train.py
@@ -16,6 +16,7 @@ from baselines.util import mpi_fork
 import experiment.click_options as main_linker
 from subprocess import CalledProcessError
 import subprocess
+from baselines.util import physical_cpu_core_count
 import wtm_envs.register_envs
 
 num_cpu = 0
@@ -105,19 +106,17 @@ def train(rollout_worker, evaluator,
 def launch(
     env, logdir, n_epochs, num_cpu, seed, policy_save_interval, restore_policy, override_params={}, save_policies=True, **kwargs):
     # Fork for multi-CPU MPI implementation.
-
     if num_cpu > 1:
         # whoami = mpi_fork(num_cpu)
-        try:
-            whoami = mpi_fork(num_cpu, ['--bind-to', 'core'])
-        except CalledProcessError:
-            # fancy version of mpi call failed, try simple version
-            whoami = mpi_fork(num_cpu)
-        # if whoami == 'parent':
-        #     whoami = mpi_fork(num_cpu)
-            # sys.exit(0)
-
-
+        n_cpus_available = physical_cpu_core_count()
+        if n_cpus_available < num_cpu:
+            whoami = mpi_fork(num_cpu) # This significantly reduces performance!
+        else:
+            try:
+                whoami = mpi_fork(num_cpu, ['--bind-to', 'core'])
+            except CalledProcessError:
+                # fancy version of mpi call failed, try simple version
+                whoami = mpi_fork(num_cpu) # This significantly reduces performance!
         if whoami == 'parent':
             sys.exit(0)
         import baselines.common.tf_util as U
