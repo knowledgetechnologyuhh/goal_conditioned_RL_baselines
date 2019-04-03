6b5cc7475a07a9248779736eef71f059f8134f92
Phuong Nguyen <pnguyen@informatik.uni-harmburg.de>
2019-04-03 09:53:30 +0200

    [scripts] mass run on wtmgws7

--- run_hyperopt_fix_params.sh
+++ run_hyperopt_fix_params.sh
@@ -16,7 +16,7 @@ krenew -K 60 -b
 env="TowerBuildMujocoEnv-sparse-gripper_random-o${n_objects}-h${min_th}-${max_th}-v1"
 
 
-for i in 1 2 3 4 5
+for i in 1 2
 do
     p_steepness='4.0'
     p_threshold='0.1'
@@ -25,6 +25,47 @@ do
 --base_logdir /data/$(whoami)/herhrl --render 0 --penalty_magnitude ${penalty_magnitude}
 --test_subgoal_perc ${test_subgoal_perc} --policies_layers [MIX_PDDL_HRL_POLICY] --mix_p_threshold ${p_threshold}
 --mix_p_steepness ${p_steepness}"
+    echo ${cmd}
+    ${cmd}
+
+
+done
+
+for i in 1 2 3 4 5
+do
+    p_steepness='4.0'
+    p_threshold='0.1'
+    cmd="python3 experiment/train.py --num_cpu ${n_cpu} --env ${env} --algorithm baselines.herhrl
+--rollout_batch_size ${rollout_batch_size} --n_epochs ${n_epochs} --n_episodes ${n_episodes}
+--base_logdir /data/$(whoami)/herhrl --render 0 --penalty_magnitude ${penalty_magnitude}
+--test_subgoal_perc ${test_subgoal_perc} --policies_layers [] --mix_p_threshold ${p_threshold}
+--mix_p_steepness ${p_steepness}"
+    echo ${cmd}
+    ${cmd}
+
+
+done
+
+for i in 1 2 3 4 5
+do
+    p_steepness='4.0'
+    p_threshold='0.1'
+    cmd="python3 experiment/train.py --num_cpu ${n_cpu} --env ${env} --algorithm baselines.herhrl
+--rollout_batch_size ${rollout_batch_size} --n_epochs ${n_epochs} --n_episodes ${n_episodes}
+--base_logdir /data/$(whoami)/herhrl --render 0 --penalty_magnitude ${penalty_magnitude}
+--test_subgoal_perc ${test_subgoal_perc} --policies_layers [PDDL_POLICY] --mix_p_threshold ${p_threshold}
+--mix_p_steepness ${p_steepness}"
+    echo ${cmd}
+    ${cmd}
+
+
+done
+
+for i in 1 2 3 4 5
+do
+    cmd="python3 experiment/train.py --num_cpu ${n_cpu} --env ${env} --algorithm baselines.her
+--rollout_batch_size ${rollout_batch_size} --n_epochs ${n_epochs} --n_episodes ${n_episodes}
+--base_logdir /data/$(whoami)/herhrl --render 0"
     echo ${cmd}
     ${cmd}
 
