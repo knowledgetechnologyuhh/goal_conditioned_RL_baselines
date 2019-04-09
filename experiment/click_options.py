240b697be567721e7ef884dfef7245ca1415fe96
6abawi <6abawi@informatik.uni-hamburg.de>
2019-04-09 18:51:48 +0200

    hook environment ready for usage

--- experiment/click_options.py
+++ experiment/click_options.py
@@ -2,7 +2,7 @@ import click
 import importlib
 
 _global_options = [
-click.option('--env', type=str, default='HookMujocoEnv-sparse-gripper_random-o3-h1-3-v1', help='the name of the OpenAI Gym environment that you want to train on'),
+click.option('--env', type=str, default='HookMujocoEnv-sparse-gripper_random-o2-h1-3-v1', help='the name of the OpenAI Gym environment that you want to train on'),
 click.option('--algorithm', default='baselines.her', help='the name of the algorithm to be used',
              type=click.Choice(['baselines.her', 'baselines.model_based', 'baselines.her_pddl', 'baselines.herhrl'])
              ),
