9e17c4771bd541e742d55f9ca6b68b9f3e9670ad
eppe <eppe@informatik.uni-hamburg.de>
2019-04-10 08:54:15 +0200

    Now testing speed

--- run_hyperopt_speed.sh
+++ run_hyperopt_speed.sh
@@ -19,40 +19,44 @@ for n_objects in 1 2
 do
     n_subgoals_layers=$(( n_objects*6 ))
     max_th=$n_objects
-    for n_episodes in 100 150 200 75
+    for n_episodes in 100
     do
-        for n_train_batches in 20 10 30 15 25
+        for n_train_batches in 15
         do
-            for i in 1 2
+            for train_batch_size in 128 256 64 512 32
             do
-
-                env="TowerBuildMujocoEnv-sparse-gripper_random-o${n_objects}-h${min_th}-${max_th}-v1"
-
-                cmd="python3 experiment/train.py
-                        --num_cpu ${n_cpu}
-                        --env ${env}
-                        --algorithm baselines.herhrl
-                    --rollout_batch_size ${rollout_batch_size}
-                    --n_epochs ${n_epochs}
-                    --n_episodes ${n_episodes}
-                    --n_train_batches ${n_train_batches}
-                    --base_logdir /data/$(whoami)/herhrl
-                    --render 0
-                    --penalty_magnitude ${penalty_magnitude}
-                    --test_subgoal_perc ${test_subgoal_perc}
-                    --policies_layers [PDDL_POLICY]
-                    --n_subgoals_layers [${n_subgoals_layers}]
-                    --early_stop_success_rate ${early_stop_threshold}
-                    --info nep${n_episodes}-tb${n_train_batches}"
-                echo ${cmd}
-                for instance in $(seq 1 $n_instances)
+                for i in 1 2
                 do
-                    ${cmd} &
-                    sleep 30
-                done
-                wait
 
-             done
+                    env="TowerBuildMujocoEnv-sparse-gripper_random-o${n_objects}-h${min_th}-${max_th}-v1"
+
+                    cmd="python3 experiment/train.py
+                            --num_cpu ${n_cpu}
+                            --env ${env}
+                            --algorithm baselines.herhrl
+                        --rollout_batch_size ${rollout_batch_size}
+                        --n_epochs ${n_epochs}
+                        --n_episodes ${n_episodes}
+                        --n_train_batches ${n_train_batches}
+                        --base_logdir /data/$(whoami)/herhrl
+                        --render 0
+                        --penalty_magnitude ${penalty_magnitude}
+                        --test_subgoal_perc ${test_subgoal_perc}
+                        --policies_layers [PDDL_POLICY]
+                        --n_subgoals_layers [${n_subgoals_layers}]
+                        --early_stop_success_rate ${early_stop_threshold}
+                        --info nep${n_episodes}-tb${n_train_batches}
+                        --train_batch_size ${train_batch_size}"
+                    echo ${cmd}
+                    for instance in $(seq 1 $n_instances)
+                    do
+                        ${cmd} &
+                        sleep 30
+                    done
+                    wait
+
+                 done
+            done
         done
     done
 done
