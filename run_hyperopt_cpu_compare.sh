e21dedbfaa4e90b2e70643d4522ee0d99557dc8c
eppe <eppe@informatik.uni-hamburg.de>
2019-04-10 10:16:37 +0200

    Now testing speed

--- run_hyperopt_cpu_compare.sh
+++ run_hyperopt_cpu_compare.sh
@@ -21,39 +21,7 @@ do
         max_th=$n_objects
         env="TowerBuildMujocoEnv-sparse-gripper_random-o${n_objects}-h${min_th}-${max_th}-v1"
         bind_core=1
-        for n_cpu in 12
-        do
-            for n_instances in 2
-            do
-                cmd="python3 experiment/train.py
-                --num_cpu ${n_cpu}
-                --env ${env}
-                --algorithm baselines.herhrl
-                --rollout_batch_size ${rollout_batch_size}
-                --n_epochs ${n_epochs}
-                --n_episodes ${n_episodes}
-                --n_train_batches ${n_train_batches}
-                --base_logdir /data/$(whoami)/herhrl
-                --render 0
-                --penalty_magnitude ${penalty_magnitude}
-                --test_subgoal_perc ${test_subgoal_perc}
-                --policies_layers [PDDL_POLICY]
-                --n_subgoals_layers [${n_subgoals_layers}]
-                --early_stop_success_rate ${early_stop_threshold}
-                --bind_core ${bind_core}
-                --info bc${bind_core}|cpu${n_instances}x${n_cpu}"
-                echo ${cmd}
-                for instance in $(seq 1 $n_instances)
-                do
-                    ${cmd} &
-                    sleep 30
-                done
-                wait
-            done
-        done
-
-        bind_core=0
-        for n_cpu in 6
+        for n_cpu in 8
         do
             for n_instances in 4
             do
@@ -83,6 +51,38 @@ do
                 wait
             done
         done
+
+#        bind_core=0
+#        for n_cpu in 6
+#        do
+#            for n_instances in 4
+#            do
+#                cmd="python3 experiment/train.py
+#                --num_cpu ${n_cpu}
+#                --env ${env}
+#                --algorithm baselines.herhrl
+#                --rollout_batch_size ${rollout_batch_size}
+#                --n_epochs ${n_epochs}
+#                --n_episodes ${n_episodes}
+#                --n_train_batches ${n_train_batches}
+#                --base_logdir /data/$(whoami)/herhrl
+#                --render 0
+#                --penalty_magnitude ${penalty_magnitude}
+#                --test_subgoal_perc ${test_subgoal_perc}
+#                --policies_layers [PDDL_POLICY]
+#                --n_subgoals_layers [${n_subgoals_layers}]
+#                --early_stop_success_rate ${early_stop_threshold}
+#                --bind_core ${bind_core}
+#                --info bc${bind_core}|cpu${n_instances}x${n_cpu}"
+#                echo ${cmd}
+#                for instance in $(seq 1 $n_instances)
+#                do
+#                    ${cmd} &
+#                    sleep 30
+#                done
+#                wait
+#            done
+#        done
 
 
     done
