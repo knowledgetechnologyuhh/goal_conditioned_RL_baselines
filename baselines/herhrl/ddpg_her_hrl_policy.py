Fixed bug with scaling and ofset in HRL prolicy.

baselines/herhrl/ddpg_her_hrl_policy.py
@@ -141,9 +141,19 @@ class DDPG_HER_HRL_POLICY(HRL_Policy):
         u = np.clip(u, -self.max_u, self.max_u)
         u += np.random.binomial(1, random_eps, u.shape[0]).reshape(-1, 1) * (self._random_action(u.shape[0]) - u)  # eps-greedy
         u = u.copy()
-        u *= self.subgoal_scale
-        u += self.subgoal_offset
         return u, q
+
+    def scale_and_offset_action(self, u):
+        scaled_u = u.copy()
+        scaled_u *= self.subgoal_scale
+        scaled_u += self.subgoal_offset
+        return scaled_u
+
+    def inverse_scale_and_offset_action(self, scaled_u):
+        u = scaled_u.copy()
+        u -= self.subgoal_offset
+        u /= self.subgoal_scale
+        return u
 
     def store_episode(self, episode_batch, update_stats=True):
 

baselines/herhrl/rollout.py
@@ -127,6 +127,7 @@ class RolloutWorker(Rollout):
 
             self.policy_action_params['success_rate'] = self.get_mean_succ_rate()
             u, q = self.policy.get_actions(o, ag, self.g, **self.policy_action_params)
+            scaled_u = self.policy.scale_and_offset_action(u)
 
             o_new = np.zeros((self.rollout_batch_size, self.dims['o']))
             ag_new = np.zeros((self.rollout_batch_size, self.dims['g']))
@@ -138,14 +139,15 @@ class RolloutWorker(Rollout):
             # Action execution
             if self.is_leaf is False:
                 if t == self.this_T-1:
-                    u = self.g.copy()  # For last step use final goal
-                self.child_rollout.g = u.copy()
-                self.child_rollout.subgoals_given[0].append(u.copy())
+                    scaled_u = self.g.copy()  # For last step use final goal
+                    u = self.policy.inverse_scale_and_offset_action(scaled_u)
+                self.child_rollout.g = scaled_u.copy()
+                self.child_rollout.subgoals_given[0].append(scaled_u.copy())
                 if not self.child_rollout.finished():
                     self.child_rollout.generate_rollouts()
             else: # In final layer execute physical action
                 for i in range(self.rollout_batch_size):
-                    self.envs[i].step(u[i])
+                    self.envs[i].step(scaled_u[i])
 
             for i in range(self.rollout_batch_size):
                 new_obs = self.envs[i].env._get_obs()