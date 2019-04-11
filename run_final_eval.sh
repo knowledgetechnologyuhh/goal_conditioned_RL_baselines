a077d4f19e4c47c2a8ffb2d684281a9de01351dd
eppe <eppe@informatik.uni-hamburg.de>
2019-04-11 18:01:42 +0200

    Added observational noise to rollout.py and as click option

--- run_final_eval.sh
+++ run_final_eval.sh
@@ -2,25 +2,27 @@
 source ./set_paths.sh
 
 n_cpu=8
-n_instances=3
+n_instances=1
 bind_core=0
 
 rollout_batch_size=1
 n_episodes=100
-n_epochs=500
+n_epochs=100
 #penalty_magnitude=1
 #test_subgoal_perc=0
 penalty_magnitude=-2
 test_subgoal_perc=1
-early_stop_threshold=98
+early_stop_threshold=75
 n_train_batches=15
 min_th=1
+
+n_objects=1
 
 krenew -K 60 -b
 
 for i in 1 2 3 4 5 
 do
-    for n_objects in 1 2 3
+    for obs_noise_coeff in '0.0' '0.01' '0.005' '0.02'
     do
         n_subgoals_layers=$(( n_objects*6 ))
         max_th=$n_objects
@@ -52,58 +54,58 @@ do
         done
         wait
 
-        cmd="python3 experiment/train.py
-        --num_cpu ${n_cpu}
-        --env ${env}
-        --algorithm baselines.herhrl
-        --rollout_batch_size ${rollout_batch_size}
-        --n_epochs ${n_epochs}
-        --n_episodes ${n_episodes}
-        --n_train_batches ${n_train_batches}
-        --base_logdir /data/$(whoami)/herhrl
-        --render 0
-        --penalty_magnitude ${penalty_magnitude}
-        --test_subgoal_perc ${test_subgoal_perc}
-        --policies_layers [DDPG_HER_HRL_POLICY]
-        --n_subgoals_layers [${n_subgoals_layers}]
-        --early_stop_success_rate ${early_stop_threshold}
-        --bind_core ${bind_core}
-        "
-#        --info bc${bind_core}|cpu${n_instances}x${n_cpu}
-        echo ${cmd}
-        for instance in $(seq 1 $n_instances)
-        do
-            ${cmd} &
-            sleep 30
-        done
-        wait
-
-        cmd="python3 experiment/train.py
-        --num_cpu ${n_cpu}
-        --env ${env}
-        --algorithm baselines.herhrl
-        --rollout_batch_size ${rollout_batch_size}
-        --n_epochs ${n_epochs}
-        --n_episodes ${n_episodes}
-        --n_train_batches ${n_train_batches}
-        --base_logdir /data/$(whoami)/herhrl
-        --render 0
-        --penalty_magnitude ${penalty_magnitude}
-        --test_subgoal_perc ${test_subgoal_perc}
-        --policies_layers []
-        --n_subgoals_layers []
-        --early_stop_success_rate ${early_stop_threshold}
-        --bind_core ${bind_core}
-        "
-#        --info bc${bind_core}|cpu${n_instances}x${n_cpu}
-        echo ${cmd}
-        for instance in $(seq 1 $n_instances)
-        do
-            ${cmd} &
-            sleep 30
-        done
-        wait
-    done
+#        cmd="python3 experiment/train.py
+#        --num_cpu ${n_cpu}
+#        --env ${env}
+#        --algorithm baselines.herhrl
+#        --rollout_batch_size ${rollout_batch_size}
+#        --n_epochs ${n_epochs}
+#        --n_episodes ${n_episodes}
+#        --n_train_batches ${n_train_batches}
+#        --base_logdir /data/$(whoami)/herhrl
+#        --render 0
+#        --penalty_magnitude ${penalty_magnitude}
+#        --test_subgoal_perc ${test_subgoal_perc}
+#        --policies_layers [DDPG_HER_HRL_POLICY]
+#        --n_subgoals_layers [${n_subgoals_layers}]
+#        --early_stop_success_rate ${early_stop_threshold}
+#        --bind_core ${bind_core}
+#        "
+##        --info bc${bind_core}|cpu${n_instances}x${n_cpu}
+#        echo ${cmd}
+#        for instance in $(seq 1 $n_instances)
+#        do
+#            ${cmd} &
+#            sleep 30
+#        done
+#        wait
+#
+#        cmd="python3 experiment/train.py
+#        --num_cpu ${n_cpu}
+#        --env ${env}
+#        --algorithm baselines.herhrl
+#        --rollout_batch_size ${rollout_batch_size}
+#        --n_epochs ${n_epochs}
+#        --n_episodes ${n_episodes}
+#        --n_train_batches ${n_train_batches}
+#        --base_logdir /data/$(whoami)/herhrl
+#        --render 0
+#        --penalty_magnitude ${penalty_magnitude}
+#        --test_subgoal_perc ${test_subgoal_perc}
+#        --policies_layers []
+#        --n_subgoals_layers []
+#        --early_stop_success_rate ${early_stop_threshold}
+#        --bind_core ${bind_core}
+#        "
+##        --info bc${bind_core}|cpu${n_instances}x${n_cpu}
+#        echo ${cmd}
+#        for instance in $(seq 1 $n_instances)
+#        do
+#            ${cmd} &
+#            sleep 30
+#        done
+#        wait
+#    done
 
 #    for p_steepness in '4.0'
 #    do
