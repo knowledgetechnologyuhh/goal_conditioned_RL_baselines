81e3f7ba4de79967d5ca4d0e11a253b9e3c59db2
eppe <eppe@informatik.uni-hamburg.de>
2019-04-12 11:41:56 +0200

    Added observational noise to rollout.py and as click option

--- run_final_eval.sh
+++ run_final_eval.sh
@@ -12,79 +12,79 @@ n_epochs=150
 #test_subgoal_perc=0
 penalty_magnitude=-2
 test_subgoal_perc=1
-early_stop_threshold=75
+early_stop_threshold=90
 n_train_batches=15
 min_th=1
 
 n_objects=1
+n_subgoals_layers=$(( n_objects*6 ))
+max_th=${n_objects}
+env="TowerBuildMujocoEnv-sparse-gripper_random-o${n_objects}-h${min_th}-${max_th}-v1"
 
 krenew -K 60 -b
 
 for i in 1 2 3 4 5 
 do
-    for obs_noise_coeff in '0.0' '0.01' '0.015'
+    for obs_noise_coeff in '0.01'
     do
-        n_subgoals_layers=$(( n_objects*6 ))
-        max_th=${n_objects}
-        env="TowerBuildMujocoEnv-sparse-gripper_random-o${n_objects}-h${min_th}-${max_th}-v1"
-
-        cmd="python3 experiment/train.py
-        --num_cpu ${n_cpu}
-        --env ${env}
-        --algorithm baselines.herhrl
-        --rollout_batch_size ${rollout_batch_size}
-        --n_epochs ${n_epochs}
-        --n_episodes ${n_episodes}
-        --n_train_batches ${n_train_batches}
-        --base_logdir /data/$(whoami)/herhrl
-        --render 0
-        --penalty_magnitude ${penalty_magnitude}
-        --test_subgoal_perc ${test_subgoal_perc}
-        --policies_layers [PDDL_POLICY]
-        --n_subgoals_layers [${n_subgoals_layers}]
-        --early_stop_success_rate ${early_stop_threshold}
-        --obs_noise_coeff ${obs_noise_coeff}
-        "
-        echo ${cmd}
-        for instance in $(seq 1 $n_instances)
-        do
-            ${cmd} &
-            sleep 30
-        done
-        wait
-
-#        for p_steepness in '4.0'
+#
+#        cmd="python3 experiment/train.py
+#        --num_cpu ${n_cpu}
+#        --env ${env}
+#        --algorithm baselines.herhrl
+#        --rollout_batch_size ${rollout_batch_size}
+#        --n_epochs ${n_epochs}
+#        --n_episodes ${n_episodes}
+#        --n_train_batches ${n_train_batches}
+#        --base_logdir /data/$(whoami)/herhrl
+#        --render 0
+#        --penalty_magnitude ${penalty_magnitude}
+#        --test_subgoal_perc ${test_subgoal_perc}
+#        --policies_layers [PDDL_POLICY]
+#        --n_subgoals_layers [${n_subgoals_layers}]
+#        --early_stop_success_rate ${early_stop_threshold}
+#        --obs_noise_coeff ${obs_noise_coeff}
+#        "
+#        echo ${cmd}
+#        for instance in $(seq 1 $n_instances)
 #        do
-#            for p_threshold in '0.35'
-#            do
-#                cmd="python3 experiment/train.py
-#                --num_cpu ${n_cpu}
-#                --env ${env}
-#                --algorithm baselines.herhrl
-#                --rollout_batch_size ${rollout_batch_size}
-#                --n_epochs ${n_epochs}
-#                --n_episodes ${n_episodes}
-#                --n_train_batches ${n_train_batches}
-#                --base_logdir /data/$(whoami)/herhrl
-#                --render 0
-#                --penalty_magnitude ${penalty_magnitude}
-#                --test_subgoal_perc ${test_subgoal_perc}
-#                --policies_layers [MIX_PDDL_HRL_POLICY]
-#                --n_subgoals_layers [${n_subgoals_layers}]
-#                --early_stop_success_rate ${early_stop_threshold}
-#                --bind_core ${bind_core}
-#                --obs_noise_coeff ${obs_noise_coeff}
-#                --mix_p_threshold ${p_threshold}
-#                --mix_p_steepness ${p_steepness}"
-#                echo ${cmd}
-#                for instance in $(seq 1 $n_instances)
-#                do
-#                    ${cmd} &
-#                    sleep 30
-#                done
-#                wait
-#            done
+#            ${cmd} &
+#            sleep 30
 #        done
+#        wait
+
+        for p_steepness in '4.0'
+        do
+            for p_threshold in '0.05' '1.0'
+            do
+                cmd="python3 experiment/train.py
+                --num_cpu ${n_cpu}
+                --env ${env}
+                --algorithm baselines.herhrl
+                --rollout_batch_size ${rollout_batch_size}
+                --n_epochs ${n_epochs}
+                --n_episodes ${n_episodes}
+                --n_train_batches ${n_train_batches}
+                --base_logdir /data/$(whoami)/herhrl
+                --render 0
+                --penalty_magnitude ${penalty_magnitude}
+                --test_subgoal_perc ${test_subgoal_perc}
+                --policies_layers [MIX_PDDL_HRL_POLICY]
+                --n_subgoals_layers [${n_subgoals_layers}]
+                --early_stop_success_rate ${early_stop_threshold}
+                --bind_core ${bind_core}
+                --obs_noise_coeff ${obs_noise_coeff}
+                --mix_p_threshold ${p_threshold}
+                --mix_p_steepness ${p_steepness}"
+                echo ${cmd}
+                for instance in $(seq 1 $n_instances)
+                do
+                    ${cmd} &
+                    sleep 30
+                done
+                wait
+            done
+        done
 
 #        cmd="python3 experiment/train.py
 #        --num_cpu ${n_cpu}
