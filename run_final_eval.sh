d128ea3554f47d8b941e0e0a4e29d1786850a3ba
eppe <eppe@informatik.uni-hamburg.de>
2019-04-15 11:28:12 +0200

    Now the number of units in AC is depending on number of objects.

--- run_final_eval.sh
+++ run_final_eval.sh
@@ -20,7 +20,8 @@ n_train_batches=15
 min_th=1
 
 n_objects=1
-n_subgoals_layers=$(( n_objects*10 ))
+#n_subgoals_layers=$(( n_objects*10 ))
+n_subgoals_layers=1
 max_th=${n_objects}
 env="TowerBuildMujocoEnv-sparse-gripper_random-o${n_objects}-h${min_th}-${max_th}-v1"
 
@@ -44,8 +45,8 @@ cmd="python3 experiment/train.py
 #for i in 1
 for i in 1 2 3
 do
-    for obs_noise_coeff in '0.0' '0.005' '0.01'
-#    for obs_noise_coeff in '0.01'
+#    for obs_noise_coeff in '0.0' '0.005' '0.01'
+    for obs_noise_coeff in '0.0'
     do
         noise_cmd="${cmd} --obs_noise_coeff ${obs_noise_coeff}"
         shallow_noise_cmd="${noise_cmd} --policies_layers [] --n_subgoals_layers []"
@@ -54,25 +55,25 @@ do
         mix_noise_cmd="${subgoal_cmd} --policies_layers [MIX_PDDL_HRL_POLICY]"
         hrl_noise_cmd="${subgoal_cmd} --policies_layers [DDPG_HER_HRL_POLICY]"
 
-        for instance in $(seq 1 $n_instances)
-        do
-            echo ${pddl_noise_cmd}
-            ${pddl_noise_cmd} &
-            sleep 30
-        done
-        wait
-
-        for p_steepness in '4.0'
-        do
-            this_mix_cmd="${mix_noise_cmd} --mix_p_steepness ${p_steepness}"
-            for instance in $(seq 1 $n_instances)
-            do
-                echo ${this_mix_cmd}
-                ${this_mix_cmd} &
-                sleep 30
-            done
-            wait
-        done
+#        for instance in $(seq 1 $n_instances)
+#        do
+#            echo ${pddl_noise_cmd}
+#            ${pddl_noise_cmd} &
+#            sleep 30
+#        done
+#        wait
+#
+#        for p_steepness in '4.0'
+#        do
+#            this_mix_cmd="${mix_noise_cmd} --mix_p_steepness ${p_steepness}"
+#            for instance in $(seq 1 $n_instances)
+#            do
+#                echo ${this_mix_cmd}
+#                ${this_mix_cmd} &
+#                sleep 30
+#            done
+#            wait
+#        done
 
         for instance in $(seq 1 $n_instances)
         do
