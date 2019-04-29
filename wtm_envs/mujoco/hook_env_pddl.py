a18033483acade35298d9900119fee1613017126
Phuong Nguyen <pnguyen@informatik.uni-harmburg.de>
2019-04-29 09:29:57 +0200

    [hook-pddl] increase the distance_threshold

--- wtm_envs/mujoco/hook_env_pddl.py
+++ wtm_envs/mujoco/hook_env_pddl.py
@@ -9,7 +9,7 @@ import copy
 class RakeObjectThresholds: #TODO: fix this
     grip_open_threshold = [0.038, 1.0]
     grip_closed_threshold = [0.0, 0.025]
-    distance_threshold = 0.015
+    distance_threshold = 0.02
     grasp_z_offset = 0.005
     rake_handle_x_offset = 2*(0.125-0.01)
     at_x_offset = 0.025
