0a1c321d1d2169a6dea48607d1a53367ae808cb8
Phuong Nguyen <pnguyen@informatik.uni-harmburg.de>
2019-06-05 16:27:57 +0200

    [script] modify run_multiple_trials for DDPG_HER_HRL_POLICY test

--- run_multiple_trials.sh
+++ run_multiple_trials.sh
@@ -4,11 +4,11 @@ source ./set_paths.sh
 n_cpu=16
 rollout_batch_size=1
 n_episodes=100
-n_epochs=500
+n_epochs=100
 n_objects=2
 min_th=1
 max_th=1
-policy="PDDL_POLICY"
+policy="DDPG_HER_HRL_POLICY"
 easy=$1
 
 
@@ -17,9 +17,10 @@ env="HookMujocoEnv-sparse-gripper_above-o${n_objects}-h${min_th}-${max_th}-e${ea
 cmd="python3 experiment/train.py --num_cpu ${n_cpu} --env ${env} --algorithm baselines.herhrl
 --rollout_batch_size ${rollout_batch_size} --n_epochs ${n_epochs} --n_episodes ${n_episodes}
 --base_logdir /data/$(whoami)/herhrl --render 0 --policies_layers [${policy}] --n_subgoals_layers [6]
---early_stop_success_rate 90 --obs_noise_coeff 0.005"
+--early_stop_success_rate 90 --obs_noise_coeff 0.0
+--penalty_magnitude 6 --test_subgoal_per 0.3"
 
-for i in 1 2 3 4 5
+for i in 1 2
 do
     krenew -K 60 -b
     echo ${cmd}
