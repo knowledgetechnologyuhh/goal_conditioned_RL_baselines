677bbdd68cddcb61bc94511f109382d3448aeb8d
meppe <eppe@informatik.uni-hamburg.de>
2019-09-03 09:18:27 +0200

    Updated set_paths.sh

--- set_paths.sh
+++ set_paths.sh
@@ -1,10 +1,10 @@
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/informatik3/wtm/home/$(whoami)/.mujoco/mjpro150/bin
 
-nv_version=$(modinfo $(find /lib/modules/$(uname -r) -iname nvidia_*.ko | head -1) | grep ^version: | cut -d' ' -f 9 | cut -d'.' -f 1)
+#nv_version=$(modinfo $(find /lib/modules/$(uname -r) -iname nvidia_*.ko | head -1) | grep ^version: | cut -d' ' -f 9 | cut -d'.' -f 1)
+#echo "Nvidia version: ${nv_version}."
+nv_version_long=$(nvidia-smi --query-gpu=driver_version --format=csv,noheader)
+nv_version=${nv_version_long:0:3}
 echo "Nvidia version: ${nv_version}."
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/nvidia-$nv_version
 export LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libGLEW.so:/usr/lib/nvidia-$nv_version/libGL.so
 
-#nv_version=418
-#export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/nvidia-$nv_version
-#export LD_PRELOAD=$LD_PRELOAD:/usr/lib/x86_64-linux-gnu/libGLEW.so:/usr/lib/nvidia-$nv_version/libGL.so
