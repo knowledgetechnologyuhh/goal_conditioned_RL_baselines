8535fab412772ee2e2bc71339fe3cdb015c05adf
meppe <eppe@informatik.uni-hamburg.de>
2019-09-06 14:34:57 +0200

    Restructured run_debug_test.sh

--- run_debug_test.sh
+++ run_debug_test.sh
@@ -8,43 +8,38 @@ n_cpu=2
 penalty_magnitude=10
 environments=( 'TowerBuildMujocoEnv-sparse-gripper_random-o2-h1-2-v1' 'TowerBuildMujocoEnv-sparse-gripper_above-o1-h1-1-v1' 'AntFourRoomsEnv-v0' )
 policy_combinations=( '[PDDL_POLICY,DDPG_HER_HRL_POLICY]' '[DDPG_HER_HRL_POLICY,DDPG_HER_HRL_POLICY]' )
+network_classes=( 'baselines.herhrl.actor_critic:ActorCritic' 'baselines.herhrl.actor_critic:ActorCriticSharedPreproc' 'baselines.herhrl.actor_critic:ActorCriticVanillaAttn' 'baselines.herhrl.actor_critic:ActorCriticVanillaAttnReduced' 'baselines.herhrl.actor_critic:ActorCriticProbSampling' 'baselines.herhrl.actor_critic:ActorCriticProbSamplingReduced' )
 action_steps_combinations=( '[4,20]' )
+
+base_cmd="python3 experiment/train.py
+                  --num_cpu ${n_cpu}
+                  --rollout_batch_size ${rollout_batch_size}
+                  --n_epochs ${n_epochs}
+                  --n_episodes ${n_episodes}
+                  --base_logdir /data/$(whoami)/herhrl
+                  --render 0
+                  --n_test_rollouts ${n_test_rollouts}
+                  --early_stop_success_rate 99"
 
 for env in "${environments[@]}"
 do
-  cmd="python3 experiment/train.py
-                --num_cpu ${n_cpu}
-                --env ${env}
-                --algorithm baselines.her
-                --rollout_batch_size ${rollout_batch_size}
-                --n_epochs ${n_epochs}
-                --n_episodes ${n_episodes}
-                --base_logdir /data/$(whoami)/herhrl
-                --render 0
-                --n_test_rollouts ${n_test_rollouts}
-                --early_stop_success_rate 99"
-                echo ${cmd}
-                ${cmd}
-  for action_steps in "${action_steps_combinations[@]}"
+  env_cmd="${base_cmd} --env ${env}"
+  for network_class in "${network_classes[@]}"
   do
-    for policy_combination in "${policy_combinations[@]}"
+    net_cmd="${env_cmd} --network_class ${network_class}"
+    her_cmd="${net_cmd} --algorithm baselines.her"
+    echo ${her_cmd}
+    ${her_cmd}
+    herhrl_cmd="${net_cmd} --algorithm baselines.herhrl"
+    for action_steps in "${action_steps_combinations[@]}"
     do
-                cmd="python3 experiment/train.py
-                --num_cpu ${n_cpu}
-                --env ${env}
-                --algorithm baselines.herhrl
-                --rollout_batch_size ${rollout_batch_size}
-                --n_epochs ${n_epochs}
-                --n_episodes ${n_episodes}
-                --base_logdir /data/$(whoami)/herhrl
-                --render 0
-                --n_test_rollouts ${n_test_rollouts}
-                --penalty_magnitude ${penalty_magnitude}
-                --policies_layers ${policy_combination}
-                --action_steps ${action_steps}
-                --early_stop_success_rate 99"
-                echo ${cmd}
-                ${cmd}
+      action_steps_cmd="${herhrl_cmd} --action_steps ${action_steps}"
+      for policy_combination in "${policy_combinations[@]}"
+      do
+        policy_comb_cmd="${action_steps_cmd} --policies_layers ${policy_combination}"
+        echo ${policy_comb_cmd}
+        ${policy_comb_cmd}
+      done
     done
   done
 done
