3db87aa2c3cc9eb857678cbeeb046adf6c8461cd
meppe <eppe@informatik.uni-hamburg.de>
2019-09-13 12:20:17 +0200

    Fixed critical error of shared preproc loss. Now running next preliminary eval script.

--- run_debug_test.sh
+++ run_debug_test.sh
@@ -4,7 +4,7 @@ source ./set_paths.sh
 python3 experiment/generate_debug_commands.py
 sleep 2
 cmd_file="debug_cmds.txt"
-max_active_procs=8
+max_active_procs=15
 cmd_ctr=0
 n_cmds=$(cat $cmd_file | wc -l)
 while IFS= read -r cmd
@@ -17,7 +17,7 @@ do
     echo "Currently, there are ${n_active_procs} active processes"
     while [ "$n_active_procs" -ge "$max_active_procs" ];do
         echo "${n_active_procs} processes running; queue is full, waiting..."
-        sleep 5
+        sleep 15
         n_active_procs=$(pgrep -c -P$$)
     done
     echo "Now executing cmd ${cmd_ctr} / ${n_cmds}: "
