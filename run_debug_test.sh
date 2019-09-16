f7b29e64c9ac2e1be4238fa788150d6d3d988584
meppe <eppe@informatik.uni-hamburg.de>
2019-09-16 18:39:53 +0200

    Fixed set_paths

--- run_debug_test.sh
+++ run_debug_test.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 source ./set_paths.sh
-
+mkdir logs || true
 python3 experiment/generate_debug_commands.py
 sleep 2
 cmd_file="debug_cmds.txt"
@@ -31,7 +31,7 @@ do
     done
     echo "Now executing cmd ${cmd_ctr} / ${n_cmds}: "
     echo ${cmd}
-    $cmd 1> debug_test_logs/${cmd_ctr}.log 2> debug_test_logs/${cmd_ctr}_err.log || true & # Execute in background
+    $cmd 1> logs/${cmd_ctr}.log 2> logs/${cmd_ctr}_err.log || true & # Execute in background
 ##    $cmd 1> logs/${cmd_ctr}.log 2> logs/${cmd_ctr}_err.log # Execute in background
     sleep 3
 done
