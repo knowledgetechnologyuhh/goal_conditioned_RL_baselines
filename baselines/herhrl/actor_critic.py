702f409f62c0c7f52ef077c2c3c54cd05ec98637
meppe <eppe@informatik.uni-hamburg.de>
2019-09-16 15:13:33 +0200

    Updated debug script with python backend

--- baselines/herhrl/actor_critic.py
+++ baselines/herhrl/actor_critic.py
@@ -52,15 +52,15 @@ class ActorCritic:
             self.preproc_in = input_pi
         with tf.variable_scope('pi'):
             self.pi_tf = self.max_u * tf.tanh(nn(
-                input_pi, [self.hidden] * (self.layers+1) + [self.dimu]))
+                input_pi, [self.hidden] * self.layers + [self.dimu]))
         with tf.variable_scope('Q'):
             # for policy training
             input_Q = tf.concat(axis=1, values=[o, g, self.pi_tf / self.max_u])
-            self.Q_pi_tf = nn(input_Q, [self.hidden] * (self.layers+1) + [1])
+            self.Q_pi_tf = nn(input_Q, [self.hidden] * self.layers + [1])
             # for critic training
             input_Q = tf.concat(axis=1, values=[o, g, self.u_tf / self.max_u])
             self._input_Q = input_Q  # exposed for tests
-            self.Q_tf = nn(input_Q, [self.hidden] * (self.layers+1) + [1], reuse=True)
+            self.Q_tf = nn(input_Q, [self.hidden] * self.layers + [1], reuse=True)
 
 class ActorCriticSharedPreproc:
     @store_args
