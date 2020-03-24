56031d5e06e973163d0be63e33c24a72e26b363e
meppe <eppe@informatik.uni-hamburg.de>
2020-03-24 16:16:23 +0100

    Updated testing script

--- set_paths.sh
+++ set_paths.sh
@@ -6,5 +6,5 @@ nv_version_long=$(nvidia-smi --query-gpu=driver_version --format=csv,noheader)
 nv_version=${nv_version_long:0:3}
 echo "Nvidia version: ${nv_version}."
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/nvidia-$nv_version
-export LD_PRELOAD=$LD_PRELOAD:/usr/lib/x86_64-linux-gnu/libGLEW.so:/usr/lib/nvidia-$nv_version/libGL.so
+export LD_PRELOAD=$LD_PRELOAD:/usr/lib/x86_64-linux-gnu/libGLEW.so
 
