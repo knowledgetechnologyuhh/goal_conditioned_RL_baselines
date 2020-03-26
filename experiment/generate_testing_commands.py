1067a322b4169e44de9b5d456958b81b436375da
meppe <eppe@informatik.uni-hamburg.de>
2020-03-26 16:00:06 +0100

    Added Mujoco BlockStacking with o objects and gripper_random goal (which is reacher task) to testing script.

--- experiment/generate_testing_commands.py
+++ experiment/generate_testing_commands.py
@@ -9,12 +9,14 @@ If you want to merge your development branch with the overall devel branch, plea
 
 class TestingConfig:
     algorithms = ['herhrl', 'her']
-    environments = ['BlockStackMujocoEnv-gripper_random-o2-v1',
-                    'BlockStackMujocoEnv-gripper_above-o1-v1',
-                    'AntFourRoomsEnv-v0', 'AntReacherEnv-v0',
-                    'HookMujocoEnv-gripper_above-e1-v1',
-                    'CausalDependenciesMujocoEnv-o1-v0',
-                    'CopReacherEnv-h1-ik1-v0']
+    environments = [
+        'BlockStackMujocoEnv-gripper_random-o0-v1',
+        'BlockStackMujocoEnv-gripper_random-o2-v1',
+        'BlockStackMujocoEnv-gripper_above-o1-v1',
+        'AntFourRoomsEnv-v0', 'AntReacherEnv-v0',
+        'HookMujocoEnv-gripper_above-e1-v1',
+        'CausalDependenciesMujocoEnv-o1-v0',
+        'CopReacherEnv-h1-ik1-v0']
 
 
 def get_her_cmds(base_cmd):
