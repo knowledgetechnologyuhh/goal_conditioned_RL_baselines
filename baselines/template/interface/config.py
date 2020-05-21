bdf11f4e1d8c3c1bdbf37c1326675d80bae6f355
6fryen <6fryen@informatik.uni-hamburg.de>
2020-05-21 15:52:37 +0200

    example algorithm which generates random actions and learns nothing

--- baselines/template/interface/config.py
+++ baselines/template/interface/config.py
@@ -5,66 +5,26 @@ import pickle
 from baselines import logger
 from gym.envs.registration import registry
 
+
 DEFAULT_ENV_PARAMS = {}
 
-DEFAULT_PARAMS = {
-    # env
-    'max_u': 1.,  # max absolute value of actions on different coordinates
-    # ddpg
-    'layers': 3,  # number of layers in the critic/actor networks
-    'hidden': 256,  # number of neurons in each hidden layers
-    'Q_lr': 0.001,  # critic learning rate
-    'pi_lr': 0.001,  # actor learning rate
-    'buffer_size': int(1E6),  # for experience replay
-    'polyak': 0.95,  # polyak averaging coefficient
-    'action_l2': 1.0,  # quadratic penalty on actions (before rescaling by max_u)
-    'clip_obs': 200.,
-    'scope': 'ddpg',  # can be tweaked for testing
-    'relative_goals': False,
-    # ddpg get actions
-    'reuse': False,
-    'use_mpi': True,
-    # training
-    'n_cycles': 50,  # per epoch
-    'rollout_batch_size': 1,  # per mpi thread
-    'n_batches': 40,  # training batches per cycle
-    'batch_size': 256,  # per mpi thread, measured in transitions and reduced to even multiple of chunk_length.
-    'n_test_rollouts': 10,  # number of test rollouts per epoch, each consists of rollout_batch_size rollouts
-    'test_with_polyak': False,  # run test episodes with the target network
-    # exploration
-    'random_eps': 0.3,  # percentage of time a random action is taken
-    'noise_eps': 0.2,  # std of gaussian noise added to not-completely-random actions as a percentage of max_u
-    # HER
-    'replay_strategy': 'future',  # supported modes: future, none
-    'replay_k': 4,  # number of additional goals used for replay, only used if off_policy_data=future
-    # normalization
-    'norm_eps': 0.01,  # epsilon used for observation normalization
-    'norm_clip': 5,  # normalized observations are cropped to this values
-}
+DEFAULT_PARAMS = {}
 
 POLICY_ACTION_PARAMS = {}
 
 CACHED_ENVS = {}
 
 ROLLOUT_PARAMS = {
-        'use_demo_states': True,
-        'policy_action_params': {'exploit': False,
-                                 'compute_Q': False,
-                                 'noise_eps': 0.2,
-                                 'random_eps': 0.3,
-                                 'use_target_net': False}
+        'policy_action_params': {}
     }
 
 EVAL_PARAMS = {
-        'use_demo_states': False,
-        'policy_action_params': {'exploit': True,
-                                 'compute_Q': True,
-                                 'noise_eps': 0.2,
-                                 'random_eps': 0.3,
-                                 'use_target_net': False
-                                 # 'use_target_net': params['test_with_polyak'],
-                                 }
+        'policy_action_params': {}
     }
+
+OVERRIDE_PARAMS_LIST = []
+
+ROLLOUT_PARAMS_LIST = ['T', 'env_name']
 
 
 def cached_make_env(make_env):
@@ -79,6 +39,26 @@ def cached_make_env(make_env):
     return CACHED_ENVS[make_env]
 
 
+def prepare_params(kwargs):
+    # policy params
+    policy_params = dict()
+
+    env_name = kwargs['env_name']
+    if env_name[:3] == 'Cop':  # use CoppeliaSim frontend if we want to render
+        registry.env_specs[env_name]._kwargs['render'] = kwargs['render']
+
+    def make_env():
+        return gym.make(env_name)
+    kwargs['make_env'] = make_env
+    tmp_env = cached_make_env(kwargs['make_env'])
+    assert hasattr(tmp_env, '_max_episode_steps')
+    kwargs['T'] = tmp_env._max_episode_steps
+    tmp_env.reset()
+    kwargs['policy_params'] = policy_params
+
+    return kwargs
+
+
 def log_params(params, logger=logger):
     for key in sorted(params.keys()):
         logger.info('{}: {}'.format(key, params[key]))
@@ -89,7 +69,21 @@ def simple_goal_subtract(a, b):
     return a - b
 
 
+def configure_policy(dims, params):
+    """configures the policy and returns it"""
+    policy = None  # SomePolicy(dims, params)
+    return policy
+
+
+def load_policy(restore_policy_file, params):
+    # Load policy.
+    with open(restore_policy_file, 'rb') as f:
+        policy = pickle.load(f)
+    return policy
+
+
 def configure_dims(params):
+    """retrieves the dimensions for observation, action and goal from the environment"""
     env = cached_make_env(params['make_env'])
     env.reset()
     obs, _, _, info = env.step(env.action_space.sample())
